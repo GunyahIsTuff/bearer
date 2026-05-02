2cfc4500505d2adc07f9ea336c14e67650a4c0ca
GunyahIsTuff <Noahjohn1414@outlook.com>
2026-05-02 21:38:30 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord import app_commands
-import requests
+import aiohttp
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -15,29 +15,42 @@ user_tokens = {}
 @client.event
 async def on_ready():
     await tree.sync()
+    await client.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game("API Running 🚀")
+    )
     print(f"Logged in as {client.user}")
 
-@tree.command(name="login", description="Generate bearer token")
+@tree.command(name="token_gen", description="Generate Auth token")
 async def login(interaction: discord.Interaction, username: str):
-    try:
-        res = requests.post(
-            f"{API_URL}/token/generator",
-            json={"username": username},
-            timeout=10
-        )
 
-        data = res.json()
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(
+                f"{API_URL}/token/generator",
+                json={"username": username},
+                timeout=10
+            ) as res:
 
-        if "token" in data:
-            user_tokens[interaction.user.id] = data["token"]
+                data = await res.json()
+
+                if "token" in data:
+                    user_tokens[interaction.user.id] = data["token"]
+
+                    await interaction.response.send_message(
+                        f"✅ Token:\n```{data['token']}```",
+                        ephemeral=True
+                    )
+                else:
+                    await interaction.response.send_message(
+                        f"❌ {data}",
+                        ephemeral=True
+                    )
+
+        except Exception as e:
             await interaction.response.send_message(
-                f"✅ Token:\n```{data['token']}```",
+                f"Error: {e}",
                 ephemeral=True
             )
-        else:
-            await interaction.response.send_message(f"❌ {data}", ephemeral=True)
-
-    except Exception as e:
-        await interaction.response.send_message(f"Error: {e}", ephemeral=True)
 
 client.run(TOKEN)
