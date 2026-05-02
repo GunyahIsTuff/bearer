02c4f0280a3d92511004f5e16fee81f6079f591d
GunyahIsTuff <Noahjohn1414@outlook.com>
2026-05-02 21:06:46 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,54 +17,6 @@ async def on_ready():
     await tree.sync()
     print(f"Logged in as {client.user}")
 
-@tree.command(name="token_gen", description="token_gen")
-async def Generate(interaction: discord.Interaction, username: str):
-    try:
-        res = requests.post(
-            f"{API_URL}/token/generator",
-            json={"username": username},
-            timeout=10
-        )
-
-        data = res.json()
-
-        if "token" in data:
-            user_tokens[interaction.user.id] = data["token"]
-            await interaction.response.send_message(
-                f"✅ Token:\n```{data['token']}```",
-                ephemeral=True
-            )
-        else:
-            await interaction.response.send_message(f"❌ {data}", ephemeral=True)
-
-    except Exception as e:
-        await interaction.response.send_message(f"Error: {e}", ephemeral=True)
-
-client.run(TOKEN)
-
-
-
-
-
-import discord
-from discord import app_commands
-import requests
-import os
-
-TOKEN = os.getenv("DISCORD_TOKEN")
-API_URL = "https://gunyahjohnvr.pythonanywhere.com/"
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
-
-user_tokens = {}
-
-@client.event
-async def on_ready():
-    await tree.sync()
-    print(f"Logged in as {client.user}")
-
 @tree.command(name="token_gen", description="Generate Auth Token")
 async def login(interaction: discord.Interaction, username: str):
     try:
